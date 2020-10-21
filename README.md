683af195dbbe83bd8913b2c999a5de1d2be81c8e
Gerasim Gerasimov <dialix@yandex.ru>
2020-10-21 20:36:45 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# event-log-sqlite
+Журнал событий на БД SQLite
+Let`s start!
+url to example: https://stackabuse.com/a-sqlite-tutorial-with-node-js/
+
+Для установки sqllite и компиляции под целевую платформу:
+npm install sqlite3 --build-from-source=sqlite3
+
---
+++ src/index.ts
@@ -0,0 +1,31 @@
+console.log('event-log-sqlite started');
+import TDAO  from './DAO';
+import EventsRepositoty from './EventsRepository';
+
+
+function main(){
+  const dao: TDAO = new TDAO('./db/database.sqlite3');
+  const eventsRepo = new EventsRepositoty(dao);
+
+  eventsRepo.createTable();
+}
+
+main()
+
+
+/*
+const express = require("express")
+const app = express()
+
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+app.use(express.static(__dirname + '/dist'));
+
+console.log('server started at 4000')
+app.listen(4000)
+*/
